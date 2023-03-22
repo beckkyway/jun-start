16399d31923f500e80253b53b5a98302a5bd35d1
Ayra <mr.space213@mail.ru>
2023-03-22 16:47:50 +0500

    new

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const langs = [
 
 document.querySelectorAll(".info__language--text").forEach (el=> {
     el.addEventListener("click",function (e){
-        langs.map(item => {
-            item.classList.remove("info__language--bold")
-        })
+        langs.map(item => item.classList.remove("info__language--bold"))
         e.target.classList.add("info__language--bold") 
     })
 } )
