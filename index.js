b5eeec5383f307334dd860469757b48d857b21e2
Ayra <mr.space213@mail.ru>
2023-03-22 16:47:07 +0500

    refactor

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const test = document.querySelectorAll(".info__language--text")
-test.forEach (el=> {
+const langs = [
+    document.getElementById("ru"),
+    document.getElementById("en")
+]
+
+document.querySelectorAll(".info__language--text").forEach (el=> {
     el.addEventListener("click",function (e){
-        console.log("e",e.target);
-        const block = e.target
-        const ruBlock = document.getElementById("ru")
-        const enBlock = document.getElementById("en")
-        ruBlock.classList.remove("info__language--bold")
-        enBlock.classList.remove("info__language--bold")
-        block.classList.add("info__language--bold") 
+        langs.map(item => {
+            item.classList.remove("info__language--bold")
+        })
+        e.target.classList.add("info__language--bold") 
     })
 } )
-
-console.log("test",test);
-
-
-
-
-
